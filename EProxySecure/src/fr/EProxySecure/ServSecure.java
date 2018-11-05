6b1449985f39c815db290ed00ddefe73f3ba03cb
Yanis <yanispc@hotmail.fr>
2018-11-05 21:30:27 +0100

    Update EProxySecure/src/fr/EProxySecure/ServSecure.java
    Update

---
+++ EProxySecure/src/fr/EProxySecure/ServSecure.java
@@ -0,0 +1,13 @@
+package fr.EProxySecure;
+
+public class ServSecure {
+	
+	public ServSecure getEProxySecure() {
+		return new ServSecure();
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
